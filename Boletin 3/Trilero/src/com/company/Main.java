0d98edcee7d9b1916e2446ee9b18073f83cbb029
PabloT90 <36712068+PabloT90@users.noreply.github.com>
2018-10-24 18:26:46 +0200

    V1.0
    Añadida funcion de apostar en el juego de Trileros. Corregidos algunos bugs y añadida documentacion necesaria

--- Boletin 3/Trilero/src/com/company/Main.java
+++ Boletin 3/Trilero/src/com/company/Main.java
@@ -1,21 +1,35 @@
-/*Descripcion: simulara el juego de un trilero.
-Entradas: respuesta, opcion cubilete.
+/*Descripcion: simulara el juego de un trilero. Desde el principio el participante cuenta con un dinero base de 100 euros.
+Entradas: respuesta, opcion cubilete, apuesta.
 Salidas: mensajes al usuario, cubiletes, cubilete con la bolita.
 Restricciones: la respuesta solo puede ser 's' o 'n'.
                la opcion del cubilete sera entre 1 y 3.
+               no puede apostar mas de lo que tiene, ni puede ser negativa la apuesta.
 ***********************
 PG
 INICIO
     LeerValidarEjecutar
     mientras quiera ejecutar
+        LeerValidarApuesta
         GenerarNumeroAleatorio
         MostrarCubiletes
         LeerValidarOpcioncubilete
         MostrarCubileteDestapado
         MostrarGanador
+        MostrarDineroTotal
         LeerValidarEjecutar
     fin_mientras
 FIN
+
+PG 2 MostrarGanador
+INICIO
+si numAleatorio == cubilete
+    MostrarMensajeGanador
+    ActualizarDineroTotal
+sino
+    MostrarMensajePerdedor
+    ActualizarDineroTotal
+FIN
+
 ***********************
 ***********************
 Estudio de Bucles
@@ -33,10 +47,16 @@ Actualizacion: Lectura final, fisicamente al final del bucle.
 Condicion Salida: respuesta == 'n'
 ____________________
 LeerValidarOpcionCubilete
-VCB: centinela
+VCB: centinela.
 Inicializacion: Lectura anticipada antes de la primera iteracion.
 Actualizacion: Lectura final, fisicamente al final del bucle.
 Condicion Salida: cubilete >= 0 && cubilete =< 3
+____________________
+LeerValidarApuesta
+VCB: centinela.
+Inicializacion: Lectura anticipada antes de la primera iteracion.
+Actualizacion: Lectura final, fisicamente al final del bucle.
+Condicion salida: (apuesta >= 0 && dineroTotal >= apuesta)
 *********************
 * */
 package com.company;
@@ -45,7 +65,7 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         char respuesta;
-        int cubilete = 0, numAleatorio=0;
+        int cubilete=0, numAleatorio=0, apuesta=0, dineroTotal=100;
         Scanner teclado = new Scanner(System.in);
         Random aleatorio = new Random();
 
@@ -56,6 +76,12 @@ public class Main {
         }while(respuesta != 's' && respuesta != 'n');
 
         while(respuesta == 's') {//mientras quiera ejecutar
+            //LeerValidadApuesta
+            do{
+                System.out.println("Tienes "+dineroTotal+", cuanto quieres apostar?");
+                apuesta = teclado.nextInt();
+            }while(apuesta < 0 || dineroTotal < apuesta); //Asi controlo cuando quiera volver a ejecutar que no mete una apuesta mayor del dinero disponible.
+
             //GenerarNumeroAleatorio
             numAleatorio = aleatorio.nextInt(3)+1;
 
@@ -98,11 +124,19 @@ public class Main {
                 System.out.println("|          |   |          |      ******   ");
                 System.out.println("|          |   |          |      ******   ");
             }
+
             //MostrarGanador
             if(numAleatorio == cubilete){
                 System.out.println("Has ganado!!");
-            }else System.out.println("Has perdido!!");
-           
+                dineroTotal = dineroTotal + apuesta;
+            }else{
+                System.out.println("Has perdido!!");
+                dineroTotal = dineroTotal - apuesta;
+            }
+
+            //MostrarDineroTotal
+            System.out.println("Tienes "+dineroTotal+" euros");
+
             //LeerValidarEjecutar
             do{
                 System.out.println("Quieres ejecutar?");
