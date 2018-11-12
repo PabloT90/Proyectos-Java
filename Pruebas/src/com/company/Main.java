dca133a83fbd1a3a0ecb134e7cd5ff5ba0ae051d
PabloT90 <36712068+PabloT90@users.noreply.github.com>
2018-11-12 17:57:53 +0100

    v1.1
    Añadida funcion para saber en cuanto tiempo haces un km segun una velocidad introducida por teclado.

---
+++ Pruebas/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+import java.util.Scanner;
+public class Main{
+    public static void main (String[] args){
+        /*Scanner teclado = new Scanner(System.in);
+        double velocidad, velocidadKM;
+        double tiempo = 0;
+
+        System.out.println("Dime un velocidad en km/h y te digo cuanto tardas en hacer 1 km.");
+        velocidad = teclado.nextInt();
+        velocidadKM = velocidad;
+
+        //Tiempo: pasar de km/h a m/s
+        velocidad = velocidad / 3.6;
+
+        //Aplicar formula e = e0 + v.t
+        tiempo = 1000/velocidad;
+        System.out.println("A "+ velocidadKM + " tardas: " + tiempo + " seg en hacer 1 KM");*/
+
+        boolean bol = false;
+
+        if(!bol)
+            System.out.println(bol);
+        else
+            System.out.println(bol);
+    }
+}
