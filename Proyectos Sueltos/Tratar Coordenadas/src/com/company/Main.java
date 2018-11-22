120cb38cef2992631d47c3b7d37c37c214cff41e
Pablo <36712068+PabloT90@users.noreply.github.com>
2018-11-22 11:13:27 +0100

    v0.5
    Boletin 06 ejercicio 9 casi terminado.

--- Proyectos Sueltos/Tratar Coordenadas/src/com/company/Main.java
+++ Proyectos Sueltos/Tratar Coordenadas/src/com/company/Main.java
@@ -1,7 +1,7 @@
 package com.company;
 /* Descripcion: se presenta un menu cuyas opciones tratan sobre coordenadas y angulos.
-   Entrada: entero coorX, entero coorY
-   Salida: logico pertenece, entero cuadrante, mensajes de comunicacion con el usuario, resultado coordenadas polares.
+   Entrada: real coorX, real coorY
+   Salida: cuadrante al que pertenecen las coordenadas,si el punto pertenece a una circunferencia de radio 10, mensajes de comunicacion con el usuario, resultado coordenadas polares.
    Restricciones: ninguna.
 
 PG
@@ -28,7 +28,10 @@ FIN
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int opcionMenu =0;
+        int opcionMenu =0, cuadrante=0;
+        double coorX=0, coorY=0;
+        boolean resultadoCaso1=true;
+        Scanner teclado = new Scanner(System.in);
         //PresentarMenu, leeryValidarOpcionMenu
         do{
             System.out.println("1- Pertenece a la recta?");
@@ -36,16 +39,32 @@ public class Main {
             System.out.println("3- Calcular coordenadas polares");
             System.out.println("4- Salir");
             opcionMenu = teclado.nextInt();
-        }while(opcionMenu< 1 || opcionMenu > 4);
+        }while(opcionMenu < 1 || opcionMenu > 4);
 
-        while(opcionMeni != 4) {//mientras opcionMenu != 4
+        while(opcionMenu != 4) {//mientras opcionMenu != 4
             switch(opcionMenu) {//segun(opcionMenu)
                 case 1://caso 1:
                     //PuntoPerteneceaCircunferencia*
+                    //LeerCoordenadas
+                    System.out.println("Dime la coordenada X");
+                    coorX = teclado.nextDouble();
+                    System.out.println("Dime la coordenada Y");
+                    coorY = teclado.nextDouble();
+                    //PerteneceACircunferencia*
+                     resultadoCaso1 = Utilidades.PuntoPerteneceaCircunferencia(coorX,coorY);
+                     //MostrarResultadoPertenceaCircunferencia
+                     System.out.println("El resultado es: "+resultadoCaso1);
                 break;//fin_caso 1
 
                 case 2://caso 2:
                     //CuadrantePunto*
+                    //LeerCoordenadas
+                    System.out.println("Dime la coordenada X");
+                    coorX = teclado.nextDouble();
+                    System.out.println("Dime la coordenada Y");
+                    coorY = teclado.nextDouble();
+                    cuadrante = Utilidades.CudrantePunto(coorX, coorY);
+                    System.out.println("El punto pertenece al: "+cuadrante +"º cuadrante.");
                 break;//fin_caso 2
 
                 case 3://caso 3:
--- Proyectos Sueltos/Tratar Coordenadas/src/com/company/Utilidades.java
+++ Proyectos Sueltos/Tratar Coordenadas/src/com/company/Utilidades.java
@@ -2,56 +2,79 @@ package com.company;
 public class Utilidades {
 
     /*PuntoPerteneceaCircunferencia
-    Entrada: entero coorX, entero coorY, entero radio.
+    Entrada: real coorX, real coorY.
     Salida: logico pertenece.
     E/S: no hay.
     Restricciones: no hay.
-    Descripcion: determina si un punto pertenece a una circunferencia de radio preestablecido.
+    Descripcion: determina si un punto pertenece a una circunferencia de radio preestablecido en 10 y centro en (0,0).
     ----------
-    Entrada: entero coorX, entero coorY, entero radio.
+    Entrada: real coorX, real coorY.
     Precondiciones: nada.
     Salida: logico pertenece.
     Postcondiciones: asociado al nombre se manda un logico. True si pertenece a la circunferencia, false en caso de que no.
+    Cabecera: logico PuntoPerteneceaCircunferencia(real coorX, real coorY)
     * */
-    public static boolean PuntoPerteneceaCircunferencia(int coorX, int coorY, int radio){
-        //Calcular distancia desde (0,0) y si es igual al radio entonces devuelve true.
-        //Necesito otra funcionalidad que cambie a positivo.
-        if(coorX < 0 || coorY < 0){
-            //Llamar a ValorAbsoluto.
-        } else{
-            //Proceder de forma normal.
-        }
-        return true;
-    }
-    /*ValorAbsoluto
-    Entrada: no hay.
-    Salida: no hay.
-    E/S: entero numero.
-    Restricciones: no hay.
-    Descripcion: calcula el valor absoluto de un numero.
-    ----------
-    Entrada: entero numero.
-    Precondiciones: no hay.
-    Salida: entero numero.
-    Postcondiciones: asociado al nombre se manda el resultado de calcular el valor absoluto.
-    * */
-    public static int calcularValorAbsoluto(int numero){
+    public static boolean PuntoPerteneceaCircunferencia(double coorX, double coorY){
+        final double distancia = 10; //Asi establezco que el radio del circulo es 10
+        final double coorXinicial = 0; final double coorYInicial = 0;
+        double distanciaPunto=0;
+        boolean pertenece = false;
 
-        return 1;
+            distanciaPunto = Distancia2Puntos(coorX,coorXinicial,coorY,coorYInicial); //Lo resuelvo calculando la distancia entre el punto origen (0,0) y el punto dado.
+
+            if(distanciaPunto == distancia || distanciaPunto >= 9.99){ //Lo he hecho asi porque al tratar con numeros reales la mayoria de las veces no da exactamente lo mismo que distancia.
+                pertenece = true;
+            }
+
+        return pertenece;
+    }
+
+    /*Distancia2Puntos
+    Entrada: real coorX1, real coorX2, real coorY1, real coorY2.
+    Salida: real distancia.
+    E/S: nada.
+    Restricciones: no hay.
+    Descripcion: calcula la distancia entre el punto (0,0) y uno dado.
+    ----------
+    Entrada: real coorX1, real coorX2, real coorY1, real coorY2.
+    Precondiciones: nada.
+    Salida: real distancia
+    Postcondiciones: asociado al nombre se manda la distancia entre los puntos.
+    Cabecera: real Distancia2Puntos(real coorX1, real coorX2, real coorY1, real coorY2)
+    * */
+    public static double Distancia2Puntos(double coorX2, double coorX1, double coorY2, double coorY1){
+        double distancia=0;
+        distancia = Math.sqrt(Math.pow(coorX2-coorX1,2)+Math.pow(coorY2-coorY1,2));
+        return distancia;
     }
 
     /*CuadrantePunto
-    Entrada:
-    Salida:
-    E/S:
-    Restricciones:
-    Descripcion:
+    Entrada: real coorX, real coorY.
+    Salida: entero cuadrante.
+    E/S: nada.
+    Restricciones: ninguna.
+    Descripcion: Nos dice en que cuandrante se encuentra un punto.
     ----------
-    Entrada:
-    Precondiciones:
-    Salida:
-    Postcondiciones:
+    Entrada: real coorX, real coorY.
+    Precondiciones: nada.
+    Salida: entero cuadrante.
+    Postcondiciones: asociado al nombre se manda en que cuadrante se encuentra el punto. Devuelve 0 si el punto se encuentra sobre un eje.
+    Cabecera: entero CuadrantePunto(real coorX, real coorY)
     * */
+    public static int CudrantePunto(double coorX, double coorY){
+        int cuadrante=0;
+
+        if(coorX > 0 && coorY > 0){
+            cuadrante = 1;
+        }else if(coorX < 0 && coorY > 0){
+            cuadrante = 2;
+        }else if(coorX <0 && coorY <0){
+            cuadrante=3;
+        }else if(coorX >0 && coorY <0){
+            cuadrante = 4;
+        }
+        return cuadrante;
+    }
 
     /*CalcularCoordenadasPolares
     Entrada:
@@ -64,6 +87,7 @@ public class Utilidades {
     Precondiciones:
     Salida:
     Postcondiciones:
+    Cabecera:
     * */
 
 }
