4ef5eda35be914bd8e07f6a9b5cce7e4350edd06
PabloT90 <36712068+PabloT90@users.noreply.github.com>
2019-03-25 20:16:57 +0100

    Presentacion br y bw
    Añadidos mas constructores para que quede mas claro.

--- Presentations/Buffered Reader y Writer/src/com/company/Main.java
+++ Presentations/Buffered Reader y Writer/src/com/company/Main.java
@@ -12,13 +12,15 @@ public class Main {
 
             //Forma abreviada
             BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\elmal\\Desktop\\ejemplos\\precentasion.txt"));
-            BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\Users\\elmal\\Desktop\\ejemplos\\precentasion.txt"));
+            BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\Users\\elmal\\Desktop\\ejemplos\\precentasion.txt", true)); //Asi no sobreescribe lo que ya hay escrito
+            //BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\Users\\elmal\\Desktop\\ejemplos\\precentasion.txt")); //Asi sobreescribe lo que haya escrito
 
             //Primero vamos a escribir lo que sea
             System.out.println("Escribe lo que quieras: ");
             bw.write(teclado.nextLine());
             bw.newLine(); //Añade una nueva linea con un separador de linea, en este caso con un ENTER
             bw.write("Esto es una prueba");
+            bw.newLine();
             bw.flush();
             bw.close();
 
